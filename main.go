47f9784fc7430f6e6d1432fd4c8c7d65e71666b7
Igor Pogorelskiy <ipogorelskiy@gmail.com>
2018-08-28 23:40:48 -0700

    Add flags

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -8,6 +10,10 @@ import (
 	"sync"
 
 	"github.com/igorpo/chatter/chatroom"
+)
+
+var (
+	port = flag.Int("port", 8000, "The port the application will run on.")
 )
 
 type templateHandler struct {
@@ -21,15 +27,17 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
+	flag.Parse()
 	r := chatroom.NewRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 	go r.Run()
-	if err := http.ListenAndServe(":8000", nil); err != nil {
-		log.Fatal("Server could not startup: ", err)
+	log.Println("Starting the server and listening on port", *port)
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", *port), nil); err != nil {
+		log.Fatalf("Server could not startup: %v", err)
 	}
 }
